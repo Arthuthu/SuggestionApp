84c011548fe97d2799b54fb6c4aadd9bf9600d1e
Arthuthu <arthurgeromello@hotmail.com>
2022-11-30 17:32:18 -0300

    Added all the models necessary for the project.

---
+++ SuggestionAppLibrary/Models/BasicSuggestionModel.cs
@@ -0,0 +1,18 @@
+﻿namespace SuggestionAppLibrary.Models;
+public class BasicSuggestionModel
+{
+   [BsonRepresentation(BsonType.String)]
+   public string Id { get; set; }
+   public string Suggestion { get; set; }
+
+   public BasicSuggestionModel()
+   {
+
+   }
+
+   public BasicSuggestionModel(SuggestionModel suggestion)
+   {
+      Id = suggestion.Id;
+      Suggestion = suggestion.Suggestion;
+   }
+}
---
+++ SuggestionAppLibrary/Models/StatusModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SuggestionAppLibrary.Models;
+public class StatusModel
+{
+   [BsonId]
+   [BsonRepresentation(BsonType.String)]
+   public string Id { get; set; }
+   public string StatusName { get; set; }
+   public string StatusDescription { get; set; }
+}
