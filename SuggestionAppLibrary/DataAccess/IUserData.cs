c9e7dba96bbb72ad5ec8f5f6181a4aec497ae34e
Arthuthu <arthurgeromello@hotmail.com>
2022-12-04 00:02:45 -0300

    DataAcces classes added

---
+++ SuggestionAppLibrary/DataAccess/IUserData.cs
@@ -0,0 +1,10 @@
+﻿namespace SuggestionAppLibrary.DataAccess;
+
+public interface IUserData
+{
+   Task CreateUser(UserModel user);
+   Task<List<UserModel>> GerUsersAsync();
+   Task<UserModel> GetUser(string id);
+   Task<UserModel> GetUserFromAuthentication(string objectId);
+   Task UpdateUser(UserModel user);
+}
